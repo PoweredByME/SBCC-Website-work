7758353c28ca72f864d09c08eb45c3e2308f2bdf
PoweredByME <saadkwi12@hotmail.com>
2017-10-29 02:00:54 +0500

    Tried adding About us div

---
+++ page_elements/index_hero_div.php
@@ -0,0 +1,35 @@
+        
+        <div class="hero-div white">
+            <div class="start-a-project-div-btn hide-on-small-only">
+                <a href="#" style="color:#191919;">
+                    <p class="start-a-project-div-btn-p" style="color:#191919;font-size:14px">
+                        <span class="start-a-project-div-btn-bar-span" style=""></span>
+                        START A PROJECT
+                    </p>
+                </a>
+            </div>
+            <div class="container">
+                <div class="center-row-div">
+                    <div class="hero-div-center-row-div-underlay">
+                        <h1 class="hero-div-center-row-div-underlay-text">Welcome</h1>
+                    </div>
+                    <div class="hero-div-center-row-div-overlay">
+                        <h1 class="hero-div-center-row-div-overlay-text">We provide <strong>solutions</strong>, <br>robust and exquisit
+                            <span class="hero-div-overlay-text-bar-span"></span>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="take-a-tour-div-button" id="take-a-tour-div-button-id" style="outline:none">
+                <a href="#take-a-tour-div-button-id" style="width:100%; text-align:center;color:#e5e5e5;display:block;cursor:url(assets/cursor.png),auto;outline:none">
+                    <p >
+                        <span>
+                            <i class="fa fa-long-arrow-down fa-2x" aria-hidden="true"></i>
+                        </span>
+                    </p>
+                </a>
+            </div>
+            
+            
+        </div>
--- page_elements/nav_bar.php
+++ page_elements/nav_bar.php
@@ -30,10 +30,11 @@
                                 <h3 class="menu-option-h3"><a class="hvr-underline-from-right" href="#">Clients</a></h3>        
                                 <h3 class="menu-option-h3"><a class="hvr-underline-from-right" href="#">Affiliation</a></h3>        
                                 <h3 class="menu-option-h3"><a class="hvr-underline-from-right" href="#">Careers</a></h3>        
+                                <h3 class="menu-option-h3 hide-on-med-and-up"><a class="hvr-underline-from-right" href="#">Start A Project</a></h3>        
                             </div>
-                            <div class="col l6 m12 s12 hide-on-med-and-down white-text" style="padding-top:100px;">
-                                <span class="menu-div-text-bar-span"></span>
-                                <p style="font-weight:200 !important" class="menu-div-text-p">
+                            <div class="col l6 m12 s12 hide-on-med-and-down" style="padding-top:100px; color: #e5e5e5">
+                                <span class="menu-div-text-bar-span" style="background-color:#e5e5e5"></span>
+                                <p style="font-weight:200 !important color:#e5e5e5; font-weight:200" class="menu-div-text-p">
                                     Humans thrive for rhythm. Art is the purest fulfilment of the yearn for harmony. One has absolute control over his art, yet what you spawn awes you, for even with complete clou
                                 </p>
                             </div>
